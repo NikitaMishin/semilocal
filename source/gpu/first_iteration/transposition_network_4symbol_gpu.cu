cd4d98934f059a09df31ac7af6052156dd284a62
NikitaMishin <mishinnikitam@gmail.com>
2021-03-20 21:36:14 +0300

    partial implementation of hyrro algrotihm on gpu with skansky adder (based on the idea from paper of kawamani)

--- source/gpu/first_iteration/transposition_network_4symbol_gpu.cu
+++ source/gpu/first_iteration/transposition_network_4symbol_gpu.cu
@@ -6,6 +6,177 @@ using namespace cooperative_groups; // or...
 using cooperative_groups::thread_group; // etc.
 
 
+namespace bitwise_prefix_lcs_cuda {
+
+
+
+    /**
+     * @tparam T
+     * @param a sum  of two sizeof(T)*8 bit unsigned vectors
+     * @param b  basically sum + 1
+     * @return  Update values for A and B and returns the  upper carry bit  for a value A
+     */
+    template <class T>
+    inline   __device__ int kawanami_sum_reduction_without_if(T &a, T &b) {
+        int with_no_carry = a > p;
+        int with_carry =  max(with_no_carry, b > p);
+
+
+        T tmp;
+        int carry_tmp;
+        int put_other_value;
+        int active_mask;
+
+        #pragma  unroll
+        for (int k = 1; k < 32; k<<=1) {
+            active_mask = (threadIdx.x % k) < k;
+
+            carry_tmp = with_no_carry;
+            tmp = a; // save them because  below we assign a possible new values for this variables
+
+            // update A part
+            put_other_value = __shfl_up_sync(0xffffffff, with_no_carry, k, k << 1); //broadcast
+            put_b_value &= active_mask;
+
+
+            a = (a & (put_other_value - 1)) | ((-put_other_value) & b);
+            with_no_carry = (~put_other_value_value & with_no_carry) |
+                            (put_other_value_value & with_carry); // if put_b_value == 1 then we put with_no_carry <- with_carry;
+
+            // update B part
+            put_other_value = !__shfl_up_sync(0xffffffff, with_carry, k, k << 1);
+            put_other_value &= active_mask ;
+            b = (tmp & (put_other_value - 1)) | ((-put_other_value) & b);
+            with_carry = (~put_other_value & carry_tmp) | (put_other_value & with_carry);
+        }
+
+        return with_no_carry;
+    }
+
+
+    /**
+    *
+    * @tparam T
+    * @param a sum  of two sizeof(T)*8 bit unsigned vectors
+    * @param b  basically sum + 1
+    * @return  Update values for a and b and returns a upper bit  for a value
+    */
+    template <class T>
+    inline   __device__ int kawanami_sum_reduction_with_if(T &a, T &b) {
+        int with_no_carry = a > p;
+        int with_carry =  max(with_no_carry, b > p);
+
+        T tmp;
+        int carry_tmp;
+        int put_other_value;
+        int active_mask;
+
+        #pragma  unroll
+        for (int k = 1; k < 32; k<<=1) {
+            active_mask = (threadIdx.x % k) < k;
+
+            carry_tmp = with_no_carry;
+            tmp = a; // save them because  below we assign a possible new values for this variables
+
+            // update A part
+            put_other_value = __shfl_up_sync(0xffffffff, with_no_carry, k, k << 1);
+            put_other_value &= active_mask ;
+
+            if (put_other_value) {
+                a = b;
+                with_no_carry = with_carry;
+            }
+
+            // update B part
+            put_other_value = !__shfl_up_sync(0xffffffff, with_carry, k, k << 1);
+            put_other_value &= active_mask;
+            if (put_other_value) {
+                b = tmp;
+                with_carry = carry_tmp;
+            }
+        }
+
+        return with_no_carry;
+    }
+
+
+
+    /**
+     * With if
+     * @tparam T
+     * @param m
+     * @param n_small | 32
+     * @param a
+     * @param lookup
+     * @param vector_v
+     * @param offset_x
+     * @param offset_y
+     * @param carries | 32
+     */
+    template <class T>
+    __global__ void hyrro_kawanami_kernel(int m, int n_small, int *a  T * lookup, T *vector_v, int offset_x, int offset_y,int *carries) {
+// todo volatile
+        __shared__ shared_carries T[32];
+        __shared__ carry_sum_yes T[32];
+        __shared__ carry_sum_no bool[32];
+        __shared__ value_A T[32];
+        __shared__ value_B T[32];
+
+        //kawanami
+        int global_id_x = offset_x + 32 * blockIdx.x + threadIdx.x;
+        int global_id_y = 1024 * (offset_y + blockId.x);
+        int global_carry_id  = 32 * (offset_y + blockId.x);
+
+        vector = vector_v[global_id_x];
+
+
+        shared_carries[threadIdx.x] = carries[global_carry_id + threadIdx.x];
+        int carry_pack = 0;
+
+        #pragma unroll
+        for (int i = 0; i < 1024; i++) {
+            //get cur carry
+
+            if(global_id_y > m) break;
+
+            int key_a = a[global_id_y];
+
+            T lookup_value = lookup[key_a * n_small + threadIdx.x];
+            T p = vector & lookup_value;
+            T sum_v = p + vector +   ;
+            T sum_v_inc = sum_v + 1;
+
+            kawanami_sum_reduction()
+
+
+
+
+            value_A[threadIdx.x] = sum_v;
+            carry_sum_no[threadIdx.x] = sum_v > p;
+            value_B[threadIdx.x] = sum_v + 1;
+            carry_sum_yes[threadIdx.x] = max(sum_v > p, (sum_v + 1) > p);
+            // reduction
+
+            int n = __shfl_up_sync(0xffffffff, value, i, 8);
+
+
+            // end todo
+
+            vector = (vector ^ p) | sum_v;
+            //carry_pack = todo;
+
+            global_id_y++;
+        }
+
+        // save 1024 bit vector back to memory
+        vector_v[global_id_x] = vector;
+        //save carries for the 1024 elements
+        carries[global_carry_id + threadIdx.x] = shared_carries[threadIdx.x];
+    }
+
+
+}
+
 
 /**
  * Fill array in pos with specified value for each cell
